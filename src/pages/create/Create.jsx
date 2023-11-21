90ca16ce68b7fa3524cd895212723d68446c8f51
mido1 <mido.15897@gmail.com>
2023-11-21 16:02:06 +0300

    First Comment - add react and react router

---
+++ src/pages/create/Create.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import  "./Create.css";
+
+const Home = () => {
+    return (
+        <div>Hello Create</div>
+    );
+}
+
+export default Home;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import  "./Home.css";
+
+const Home = () => {
+    return (
+        <div>Hello Home</div>
+    );
+}
+
+export default Home;
